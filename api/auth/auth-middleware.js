043e80359d070c1ee6527c9d9e11715699a00c5a
stkirk <shaanekirk@gmail.com>
2021-10-29 13:09:08 -0400

    find users db model function working with get users endpoint

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -16,9 +16,11 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
-}
+  console.log("restricting endpoint...");
+  next();
+};
 
-const only = role_name => (req, res, next) => {
+const only = (role_name) => (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
@@ -29,8 +31,7 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
-}
-
+};
 
 const checkUsernameExists = (req, res, next) => {
   /*
@@ -40,8 +41,7 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
-}
-
+};
 
 const validateRoleName = (req, res, next) => {
   /*
@@ -62,11 +62,11 @@ const validateRoleName = (req, res, next) => {
       "message": "Role name can not be longer than 32 chars"
     }
   */
-}
+};
 
 module.exports = {
   restricted,
   checkUsernameExists,
   validateRoleName,
   only,
-}
+};
